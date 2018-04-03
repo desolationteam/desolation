db8774bc2e28ec03fdc7823089292436a583acc8
Dzianis Reznik <dzianis.reznik@gmail.com>
2018-04-03 18:34:02 +0300

    Webpack settings fix

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,89 +1,91 @@
 import path from 'path';
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import UglifyJSPlugin from 'uglifyjs-webpack-plugin';
 
 module.exports = {
-  entry: {
-    app: './src/index.js'
-  },
-  output: {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: {minimize: true}
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          {
-            loader: 'css-loader',
-            options: {sourceMap: true}
-          }
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-            options: {sourceMap: true}
-          },
-          {
-            loader: 'sass-loader',
-            options: {sourceMap: true}
-          }
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].[hash].css"
-    }),
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  ],
-  devtool: 'source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  }
+	entry: {
+		app: './src/index.js'
+	},
+	output: {
+		filename: '[name].[hash].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: {minimize: true}
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					{
+						loader: 'css-loader',
+						options: {sourceMap: true}
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					{
+						loader: 'css-loader',
+						options: {sourceMap: true}
+					},
+					{
+						loader: 'sass-loader',
+						options: {sourceMap: true}
+					}
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: './index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].[hash].css'
+		}),
+		new UglifyJSPlugin({
+			sourceMap: true
+		}),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		})
+	],
+	devtool: 'source-map',
+	devServer: {
+		contentBase: './dist',
+		hot: true
+	}
 };
