bfc47ad907ac582d5de09e5b934ddbee5b99e913
Dzianis Reznik <dzianis.reznik@gmail.com>
2018-05-16 04:24:37 +0300

    kek

--- src/scripts/Player.js
+++ src/scripts/Player.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-import PointerLockControls from './PointerLockControls';
+import Controls from './Controls';
 
 export default class Player {
 	constructor(socket, scene, camera) {
@@ -9,12 +9,12 @@ export default class Player {
 		this.mesh = new THREE.Mesh(geometry, material);
 		this.mesh.add(camera);
 		camera.position.set(0,8,20);
-		this.controls = new PointerLockControls(this.mesh);
-		scene.add(this.controls.getObject());
+		this.controls = new Controls(this.mesh);
+		scene.add(this.controls.mesh);
 		this.socket = socket;
 		this.socket.emit('new player', {
 			state: {
-				position: this.controls.getObject().position
+				position: this.controls.mesh.position
 			}
 		});
 	}
@@ -24,44 +24,46 @@ export default class Player {
 	}
 
 	updateMotion() {
-		let movement = this.controls.movement;
+		if (this.controls.controlsEnabled) {
+			let movement = this.controls.movement;
 
-		if (movement.forward || movement.backward || movement.left || movement.right){
-			const velocity = new THREE.Vector3();
+			if (movement.forward || movement.backward || movement.left || movement.right) {
+				const velocity = new THREE.Vector3();
 
-			if (movement.forward) {
-				velocity.z -= 1;
-			}
-			if (movement.backward) {
-				velocity.z += 1;
-			}
-			if (movement.left){
-				velocity.x -= 1;
-			}
-			if (movement.right){
-				velocity.x += 1;
-			}
-
-			if (velocity.x) {
-				this.controls.getObject().translateX(velocity.x);
-			}
-			if (velocity.y) {
-				this.controls.getObject().translateY(velocity.y);
-			}
-			if (velocity.z) {
-				this.controls.getObject().translateZ(velocity.z);
-			}
-			if (this.controls.getObject().position.y < 10) {
-				velocity.y = 0;
-				this.controls.getObject().position.y = 10;
-			}
-
-			this.socket.emit('move', {
-				state:{
-					position: this.controls.getObject().position,
-					rotation: this.controls.getObject().rotation
+				if (movement.forward) {
+					velocity.z -= 1;
 				}
-			});
+				if (movement.backward) {
+					velocity.z += 1;
+				}
+				if (movement.left) {
+					velocity.x -= 1;
+				}
+				if (movement.right) {
+					velocity.x += 1;
+				}
+
+				if (velocity.x) {
+					this.controls.mesh.translateX(velocity.x);
+				}
+				if (velocity.y) {
+					this.controls.mesh.translateY(velocity.y);
+				}
+				if (velocity.z) {
+					this.controls.mesh.translateZ(velocity.z);
+				}
+				if (this.controls.mesh.position.y < 10) {
+					velocity.y = 0;
+					this.controls.mesh.position.y = 10;
+				}
+
+				this.socket.emit('move', {
+					state: {
+						position: this.controls.mesh.position,
+						rotation: this.controls.mesh.rotation
+					}
+				});
+			}
 		}
 	}
 }
