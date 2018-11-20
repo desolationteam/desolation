6d863634313633545f64c6e8217d8f278a7e100e
Dzmitry_Prudnikau <dzmitriyprudnikov@gmail.com>
2018-11-20 23:12:36 +0300

    refactor server.js

--- server.js
+++ server.js
@@ -2,89 +2,60 @@ const path = require('path');
 const express = require('express');
 const compression = require('compression');
 const socketIO = require('socket.io');
+const server = require('http');
+
 const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(compression());
-const server = require('http').createServer(app);
+
+server.createServer(app);
 const io = socketIO(server);
 server.listen(process.env.PORT || 5000);
 
-let playersCount = 0;
 const connections = [];
 
+let amountOfPlayers = 0;
+
 io.on('connection', socket => {
-	playersCount++;
-	socket.index = playersCount;
+	socket.index = ++amountOfPlayers;
 	connections.push(socket);
 	setListeners(socket);
 });
 
 function setListeners(socket) {
 	socket.on('new player', data => {
-		socket.playerData = Object.assign({}, data, {index: socket.index});
-		io.emit('receive message', {
-			type: 'connect',
-			nickname: socket.playerData.nickname
-		});
-		const filtered = connections.filter(connection => connection.index !== socket.index);
-		filtered.forEach(connection => {
-			connection.emit('create player', socket.playerData);
-		});
-		const playersData = filtered.map(player => {
-			if (player.playerData) return player.playerData;
-		});
-		playersData.forEach(data => socket.emit('create player', data));
+		io.emit('receive message', { type: 'connect', nickname: data.nickname });
+		socket.playerData = Object.assign({}, data, { index: socket.index });
+		connections.filter(connection => connection.index !== socket.index)
+			.forEach(connection => connection.emit('create player', socket.playerData))
+			.map(player => player.playerData)
+			.forEach(data => socket.emit('create player', data));
 	});
 
 	socket.on('enemy hitted', data => {
-		const filtered = connections.filter(connection => connection.index !== socket.index);
-		filtered.forEach(connection => {
-			connection.emit('hit player', data);
-		});
+		connections.filter(connection => connection.index !== socket.index)
+			.forEach(connection => connection.emit('hit player', data));
 	});
 
 	socket.on('enemy death', data => {
-		const filtered = connections.filter(connection => connection.index !== socket.index);
-		filtered.forEach(connection => {
-			connection.emit('kill player', data);
-		});
-		io.emit('receive message', {
-			type: 'death',
-			nickname: data.enemyName,
-			killer: data.killer,
-		});
+		io.emit('receive message', { type: 'death', nickname: data.enemyName, killer: data.killer });
+		connections.filter(connection => connection.index !== socket.index)
+			.forEach(connection => connection.emit('kill player', data));
 	});
 
 	socket.on('move', data => {
-		socket.playerData = Object.assign({}, data, {index: socket.index});
-		const filtered = connections.filter(connection => connection.index !== socket.index);
-		filtered.forEach(connection => connection.emit('update player', socket.playerData));
+		socket.playerData = Object.assign({}, data, { index: socket.index });
+		connections.filter(connection => connection.index !== socket.index)
+			.forEach(connection => connection.emit('update player', socket.playerData));
 	});
 
 	socket.on('send message', data => {
-		connections.forEach(connection => {
-			connection.emit('receive message', {
-				type: 'message',
-				nickname: data.nickname,
-				text: data.text
-			});
-		});
-	});
-
-	socket.on('talk', (message) => {
-		const filtered = connections.filter(connection => connection.index !== socket.index);
-		filtered.forEach(connection => connection.emit('talk', message));
+		connections.forEach(connection => connection.emit('receive message', { type: 'message', nickname: data.nickname, text: data.text }));
 	});
 
 	socket.on('disconnect', () => {
-		if (socket.playerData && socket.playerData.nickname) {
-			const nickname = socket.playerData.nickname;
-			io.emit('receive message', {
-				type: 'disconnect',
-				nickname: nickname
-			});
-		}
-		connections.splice(connections.findIndex(connection => connection.index === socket.index), 1);
 		io.emit('remove player', socket.index);
+		socket.playerData && socket.playerData.nickname && io.emit('receive message', { type: 'disconnect', nickname: socket.playerData.nickname });
+		connections.splice(connections.findIndex(connection => connection.index === socket.index), 1);
 	});
 }
